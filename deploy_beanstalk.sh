b1ecfe057dfa32ede31f5e5e729e6354759a72c4
Paul Headington <paul.headington@colensobbdo.co.nz>
2014-08-18 16:48:16 +1200

    updated deploy script

--- deploy_beanstalk.sh
+++ deploy_beanstalk.sh
@@ -8,10 +8,10 @@ git clean -fd
 
 cd site/
 
-# precompile assets, ...
+# precompile assets, ..
 
 # zip the application
-zip -x *.git* -r "${APP_NAME}-${APP_VERSION}.zip" .
+zip -x *.git* -r "${APP_NAME}-${APP_VERSION}.zip"
 
 # delete any version with the same name (based on the short revision)
 aws elasticbeanstalk delete-application-version --application-name "${APP_NAME}" --version-label "${APP_VERSION}"  --delete-source-bundle
@@ -19,6 +19,7 @@ aws elasticbeanstalk delete-application-version --application-name "${APP_NAME}"
 # upload to S3
 aws s3 cp ${APP_NAME}-${APP_VERSION}.zip s3://${S3_BUCKET}/${APP_NAME}-${APP_VERSION}.zip
 
+
 # create a new version and update the environment to use this version
-aws elasticbeanstalk create-application-version --application-name "${APP_NAME}" --version-label "${APP_VERSION}" --source-bundle S3Bucket="${S3_BUCKET}",S3Key="${APP_NAME}-${APP_VERSION}.zip"
-aws elasticbeanstalk update-environment --environment-name "${ENV_NAME}" --version-label "${APP_VERSION}"
+#aws elasticbeanstalk create-application-version --application-name "${APP_NAME}" --version-label "${APP_VERSION}" --source-bundle S3Bucket="${S3_BUCKET}",S3Key="${APP_NAME}-${APP_VERSION}.zip"
+#aws elasticbeanstalk update-environment --environment-name "${ENV_NAME}" --version-label "${APP_VERSION}"
